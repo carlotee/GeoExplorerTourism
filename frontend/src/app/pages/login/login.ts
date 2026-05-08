6b201a78f2504c2ad18d08b915533608d20d478e
Carlos Vega <carlosvega3031@gmail.com>
2026-05-08 03:27:44 -0400

    se agrega el login + su css del login

--- frontend/src/app/pages/login/login.ts
+++ frontend/src/app/pages/login/login.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css',
+  styleUrls: ['./login.css']
 })
 export class Login {
+
+  email: string = '';
+  password: string = '';
+
+  iniciarSesion() {
+    console.log('Login funcionando');
+
+    console.log(this.email);
+    console.log(this.password);
+  }
 
 }
