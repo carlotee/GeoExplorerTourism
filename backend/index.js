573850d31e98a09352d15264fc712771aa53b50d
Carlos Vega <carlosvega3031@gmail.com>
2026-05-23 03:31:05 -0400

    se crea un base en el dashboard y se crea los pages de carrito y reservas

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet'); s
+const helmet = require('helmet'); 
 const morgan = require('morgan'); 
 require('dotenv').config();
 
