926c251cd7bcaad010f3e7219c2192565a5970f9
Carlos Vega <carlosvega3031@gmail.com>
2026-08-04 02:32:27 -0400

    se actualiza el usuarios.ts

--- frontend/src/app/services/usuarios.ts
+++ frontend/src/app/services/usuarios.ts
@@ -6,13 +6,19 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UsuariosService {
-  
+
   // Ajusta el puerto '3000' si tu backend (el index.js) corre en uno distinto
-  private apiUrl = 'http://localhost:3000/api/usuarios'; 
+  private apiUrl = 'http://localhost:3000/api/usuarios';
 
   constructor(private http: HttpClient) { }
 
   obtenerTodos(): Observable<any> {
     return this.http.get(this.apiUrl);
+  }
+  editarUsuario(id: number, usuario: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, usuario);
+  }
+  eliminarUsuario(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
   }
 }
